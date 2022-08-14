0fb86f4b0f2d8f07920bdf3b0d89225e5211a028
ClevelandSchemer <samjsmith1996@gmail.com>
2022-08-13 20:58:37 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sstools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
