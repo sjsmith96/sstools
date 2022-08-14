0b37b18308776bfc32c4ed3f9510c87e7a8cc05b
ClevelandSchemer <samjsmith1996@gmail.com>
2022-08-13 21:49:21 -0400

    That was easy

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
+using PgpCore;
 
 namespace sstools
 {
+
+    public class SSEncrypt
+    {
+        public SSEncrypt()
+        {
+        }
+        
+        private const string TEST_KEY_PATH = @"C:\users\sam\desktop\test.asc";
+        private const string TEST_INPUT_PATH = @"C:\users\sam\desktop\pgptest.zip";
+        private const string TEST_OUTPUT_PATH = @"C:\users\sam\desktop\testes.pgp";
+        
+        public void PgpEncrypt()
+        {
+            FileInfo publicKey = new FileInfo(TEST_KEY_PATH);
+            EncryptionKeys encryptionKeys = new EncryptionKeys(publicKey);
+
+            FileInfo inputFile = new FileInfo(TEST_INPUT_PATH);
+            FileInfo outputFile = new FileInfo(TEST_OUTPUT_PATH);
+
+            PGP pgp = new PGP(encryptionKeys);
+            pgp.EncryptFile(inputFile, outputFile);
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SSEncrypt test = new SSEncrypt();
+            test.PgpEncrypt();
         }
     }
 }
