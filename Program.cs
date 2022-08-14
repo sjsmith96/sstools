4f10b26a4c6c7efb0bfbcf7c031748640a1fb36c
ClevelandSchemer <samjsmith1996@gmail.com>
2022-08-14 10:33:20 -0400

    Cleaned up a bit

--- Program.cs
+++ Program.cs
@@ -4,24 +4,18 @@ using PgpCore;
 
 namespace sstools
 {
-
-    public class SSEncrypt
+    public class SSTools
     {
-        public SSEncrypt()
-        {
-        }
+        public SSTools()
+        { }
         
-        private const string TEST_KEY_PATH = @"C:\users\sam\desktop\test.asc";
-        private const string TEST_INPUT_PATH = @"C:\users\sam\desktop\pgptest.zip";
-        private const string TEST_OUTPUT_PATH = @"C:\users\sam\desktop\testes.pgp";
-        
-        public void PgpEncrypt()
+        public void PgpEncrypt(string keyPath, string input, string output)
         {
-            FileInfo publicKey = new FileInfo(TEST_KEY_PATH);
+            FileInfo publicKey = new FileInfo(keyPath);
             EncryptionKeys encryptionKeys = new EncryptionKeys(publicKey);
 
-            FileInfo inputFile = new FileInfo(TEST_INPUT_PATH);
-            FileInfo outputFile = new FileInfo(TEST_OUTPUT_PATH);
+            FileInfo inputFile = new FileInfo(input);
+            FileInfo outputFile = new FileInfo(output);
 
             PGP pgp = new PGP(encryptionKeys);
             pgp.EncryptFile(inputFile, outputFile);
@@ -29,11 +23,15 @@ namespace sstools
     }
     
     class Program
-    {
+    {                            
+        private const string TEST_KEY_PATH = @"C:\users\sam\desktop\test.asc";
+        private const string TEST_INPUT_PATH = @"C:\users\sam\desktop\pgptest.zip";
+        private const string TEST_OUTPUT_PATH = @"C:\users\sam\desktop\testes.pgp";
+        
         static void Main(string[] args)
         {
-            SSEncrypt test = new SSEncrypt();
-            test.PgpEncrypt();
+            SSTools tools = new SSTools();
+            tools.PgpEncrypt(TEST_KEY_PATH, TEST_INPUT_PATH, TEST_OUTPUT_PATH);
         }
     }
 }
